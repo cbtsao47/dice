d141963dfd6dbda1bf2e21c6bd29f612a546ff4d
wisdomtooth <cbtsao47@gmail.com>
2018-12-15 21:13:47 -0600

    add reset button

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const button = document.querySelector(".dice__trigger");
 const img = document.querySelector(".dice__img");
 const imgWrapper = document.querySelector(".dice__result");
-const reset = function() {
+const restart = document.querySelector(".dice__restart");
+
+const reset = function(event) {
   console.log("hi");
   TweenMax.to(".dice__result", 0, {
-    rotation: 0
+    rotation: 0,
+    y: 0
   });
 };
 const changeDice = function(event) {
   TweenMax.to(".dice__result", 3, {
-    rotation: 720,
-    onComplete: reset
+    y: 500,
+    ease: Bounce.easeOut,
+    rotation: 900
   });
   for (let i = 1; i <= 20; i++) {
     setTimeout(() => {
@@ -26,3 +30,4 @@ const changeDice = function(event) {
 };
 
 button.addEventListener("click", event => changeDice(event));
+restart.addEventListener("click", event => reset(event));
