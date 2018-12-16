47b3e95c9b4aeee579839bf1c2ed454d94d52b32
wisdomtooth <cbtsao47@gmail.com>
2018-12-15 20:53:09 -0600

    add spin on click animation

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const button = document.querySelector(".dice__trigger");
 const img = document.querySelector(".dice__img");
-
+const imgWrapper = document.querySelector(".dice__result");
+const reset = function() {
+  console.log("hi");
+  TweenMax.to(".dice__result", 0, {
+    rotation: 0
+  });
+};
 const changeDice = function(event) {
+  TweenMax.to(".dice__result", 3, {
+    rotation: 720,
+    onComplete: reset
+  });
   for (let i = 1; i <= 20; i++) {
     setTimeout(() => {
       let imgNumber = Math.ceil(Math.random() * 6);
